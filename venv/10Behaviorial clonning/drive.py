1d57f1a5ba940215107dd99760613c71af7cb07f
kartikeyshaurya@yahoo.com <kartikeyshaurya@yahoo.com>
2019-10-12 23:06:17 +0530

    added new code that decrease the error in self driving car

--- venv/10Behaviorial clonning/drive.py
+++ venv/10Behaviorial clonning/drive.py
@@ -9,25 +9,29 @@ from io import BytesIO
 from PIL import Image
 import cv2
 
-
-
 sio = socketio.Server()
+
 app = Flask(__name__)
 
 def  img_preprocess(img):
     img = img[60:135,:,:]
     img = cv2.cvtColor(img,cv2.COLOR_RGB2YUV)
-    img = cv2.resize(img, (200, 66))
+    img = cv2.resize(img, (320, 75))   # changes from 320 ,3 -->> 200,3
+    img
     img = img/255
     return img
 
 @sio.on('telemetry')
 def telemetry(sid, data):
+    speed = float(data['speed'])
     image = Image.open(BytesIO(base64.b64decode(data['image'])))
     image = np.asarray(image)
     image = img_preprocess(image)
     image = np.array([image])
     steering_angle = float(model.predict(image))
+    throttle = 1.0 - speed/speed_limit
+    print('{} {} {}'.format(steering_angle, throttle, speed))
+    send_control(steering_angle, throttle)
     send_control(steering_angle, 1.0)
 
 
--- venv/10Behaviorial clonning/drive2.py
+++ venv/10Behaviorial clonning/drive2.py
@@ -16,7 +16,7 @@ def img_preprocess(img):
     img = img[60:135,:,:]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
     img = cv2.GaussianBlur(img,  (3, 3), 0)
-    img = cv2.resize(img, (200, 66))
+    img = cv2.resize(img, (320, 75))
     img = img/255
     return img
 
