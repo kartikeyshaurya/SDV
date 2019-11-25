404e2249722ba90d026cb52af4dfc1c8f341c424
kartikeyshaurya@yahoo.com <kartikeyshaurya@yahoo.com>
2019-11-25 15:47:05 +0530

    modified the files to work properly

--- venv/2The_Perceptron/perceptron_final.py
+++ venv/2The_Perceptron/perceptron_final.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-#%matplotlib inline
 
 def draw(x1,x2):
     ln = plt.plot(x1,x2, '-')
@@ -31,16 +30,20 @@ def gradient_descent(line_parameters, points, y , alpha):
         draw(x1,x2)
         print(calculate_error(line_parameters, points,y))
 
-n_pts = 100
+n_pts=100
 np.random.seed(0)
-bias = np.ones(n_pts)
-top_region = np.array([np.random.normal(10, 2, n_pts), np.random.normal(12, 2, n_pts), bias]).T
-bottom_region = np.array([np.random.normal(5, 2, n_pts), np.random.normal(6, 2, n_pts), bias]).T
-all_points = np.vstack((top_region, bottom_region))
-line_parameters = np.matrix(np.zeros(3)).T
-y = np.array([(np.zeros(n_pts), np.ones(n_pts))]).reshape(n_pts*2, 1)
-ax = plt.subplots(figsize=(4, 4))
-y.scatter(top_region[:, 0], top_region[:, 1], color='r')
-ax.scatter(bottom_region[:, 0], bottom_region[:, 1], color='b')
-draw(x1, x2)
+bias= np.ones(n_pts)
+top_region=np.array([np.random.normal(10,2,n_pts), np.random.normal(12,2,n_pts), bias]).T
+bottom_region= np.array([np.random.normal(5,2, n_pts), np.random.normal(6,2, n_pts), bias]).T
+all_points=np.vstack((top_region, bottom_region))
+
+line_parameters = np.matrix([np.zeros(3)]).T
+# x1=np.array([bottom_region[:,0].min(), top_region[:,0].max()])
+# x2= -b/w2 + (x1*(-w1/w2))
+y=np.array([np.zeros(n_pts), np.ones(n_pts)]).reshape(n_pts*2, 1)
+
+_, ax= plt.subplots(figsize=(4,4))
+ax.scatter(top_region[:,0], top_region[:,1], color='r')
+ax.scatter(bottom_region[:,0], bottom_region[:,1], color='b')
+gradient_descent(line_parameters, all_points, y , 0.06)
 plt.show()
